9f08c90fbcef0ceaecb7cacf536d872c9920f27b
semkaas <6052414@mborijnland.nl>
2026-05-08 10:38:53 +0200

    footer gemaakt

--- includes/footer.php
+++ includes/footer.php
@@ -1,1 +1,6 @@
-
+<article class="voet">
+    <section class="sec1"><img class="logo" src="img/logo.png"></section>
+    <section class="sec2"><h1 class="voettekst">kaas2</h1></section>
+    <section class="sec3"><h1 class="voettekst">kaas3</h1></section>
+    <section class="sec4"><h1 class="voettekst">kaas4</h1></section>
+</article>
