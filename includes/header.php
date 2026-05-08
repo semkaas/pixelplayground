a8db5ba6e43ee523464cf43e21a0c083950a3ffc
semkaas <6052414@mborijnland.nl>
2026-05-08 10:13:21 +0200

    header gemaakt

--- includes/header.php
+++ includes/header.php
@@ -1,9 +1,10 @@
 <article class="banner1">
+    <img class="logo" src="img/logo.png">
+    <h1 class="welkom">Welcome</h1>
     <nav class="nav1">
-        <a href="#">Welcome</a>
         <a href="#">Home</a>
         <a href="#">Games</a>
         <a href="#">Login</a>
-        <a href="#">register</a>
+        <a href="#">Register</a>
     </nav>
 </article>
