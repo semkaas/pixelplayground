f557785ec87d9c470914ee7ea35911f215a7988e
semkaas <6052414@mborijnland.nl>
2026-05-08 09:38:11 +0200

    nav gemaakt

---
+++ includes/header.php
@@ -0,0 +1,9 @@
+<article class="banner1">
+    <nav class="nav1">
+        <a href="#">Welcome</a>
+        <a href="#">Home</a>
+        <a href="#">Games</a>
+        <a href="#">Login</a>
+        <a href="#">register</a>
+    </nav>
+</article>
