f65a44bcd3e04946284ef693816c55515f1b0667
semkaas <6052414@mborijnland.nl>
2026-06-04 10:41:06 +0200

    oui

--- includes/headerloggedin.php
+++ includes/headerloggedin.php
@@ -3,7 +3,7 @@ session_start();
 ?>
 <article class="banner2">
     <img class="logo" src="img/logo.png">
-    <h1 class="welkom">Welcome, <?php echo $_SESSION['username']; ?></h1>
+    <h1 class="welkom">Welcome, <?php echo $_SESSION['username'] ?? "Newbie"; ?></h1>
     <nav class="nav2">
         <a href="indexlogin.php">Home</a>
         <a href="gameslogin.php">Games</a>
